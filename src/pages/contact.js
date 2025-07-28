c0f441ebd329423acfca1085218ebd134e331e0e
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2025-07-28 10:22:57 -0400

    contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,109 @@
 import React from "react"
 import * as styles from "../components/contact.module.css"
+import { motion } from "motion/react"
 
 const Contact = () => {
   return (
     <div className="margined-section">
       <h1 className={styles.title}>Contact</h1>
+      <motion.div
+        initial={{ opacity: 0 }}
+        transition={{ duration: 1 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true }}
+        className={styles.infoGrid}
+      >
+        <motion.div
+          initial={{ opacity: 0 }}
+          transition={{ duration: 1 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true }}
+          className={styles.column}
+        >
+          <p>Solid Objectives Idenburg Liu</p>
+          <p>
+            New York
+            <br /> <a href="tel:17186246666">718 624 6666</a>
+            <br />
+            <a
+              href="https://maps.app.goo.gl/5srGVMNrxpvagUTs5"
+              target="_blank"
+              rel="noreferrer"
+            >
+              42 Adelphi St
+              <br />
+              Brooklyn, NY 11205
+            </a>
+          </p>
+          <p>
+            Amsterdam
+            <br />
+            <a href="tel:234 234 5698">234 234 5698</a>
+            <br />
+            <a
+              href="https://maps.app.goo.gl/TP6SZtRyZpz5zeN77"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Anne Frankstraat
+              <br />
+              234 1018 BZ
+            </a>
+          </p>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          transition={{ duration: 1 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true }}
+          className={styles.column}
+        >
+          <p>
+            General
+            <br />
+            <a href="mailto:office@so-il.org">office@so-il.org</a>
+            <br />
+            <a href="tel:17186246666">718 624 6666</a>
+          </p>
+          <p>
+            Press <br />
+            <a href="mailto:press@so-il.org">press@so-il.org</a>
+          </p>
+          <p>
+            New Business <br />
+            <a href="mailto:collaborate@so-il.org">collaborate@so-il.org</a>
+          </p>
+          <p>
+            Employment Inquiries
+            <br />
+            <a href="mailto:work@so-il.org">work@so-il.org</a>
+          </p>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          transition={{ duration: 1 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true }}
+          className={styles.column}
+        >
+          <a href="https://google.com" target="_blank" rel="noreferrer">
+            Instagram
+          </a>
+          <br />
+          <a href="https://google.com" target="_blank" rel="noreferrer">
+            Linkedin
+          </a>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          transition={{ duration: 1 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true }}
+          className={styles.column}
+        >
+          Newsletter
+        </motion.div>
+      </motion.div>
     </div>
   )
 }
