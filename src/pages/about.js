298ebe16d1884c270b8c1b38ea935dbc9470478d
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2025-09-04 13:01:37 -0400

    sticky about nav

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useRef, useEffect } from "react"
 import { graphql } from "gatsby"
 import { GatsbyImage } from "gatsby-plugin-image"
 import * as styles from "../components/about.module.css"
@@ -29,180 +29,275 @@ const About = ({ data }) => {
     studioText,
     transparency,
   } = data.contentfulAboutPage
-  const [activeVideo, setActiveVideo] = useState()
+  const studioRef = useRef(null)
+  const teamRef = useRef(null)
+  const clientsRef = useRef(null)
+  const collectionsRef = useRef(null)
+  const recognitionRef = useRef(null)
+  const transparencyRef = useRef(null)
+  const careersRef = useRef(null)
+  const contactRef = useRef(null)
+  const navRef = useRef(null)
+  const [activeSection, setActiveSection] = useState("studio")
+  const [isFixed, setIsFixed] = useState(false)
   const { height, width } = useWindowSize()
   const isMobile = height > width
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          setActiveSection(entry.target.id)
+        }
+      })
+    })
+
+    // Observe each section
+    if (studioRef.current) observer.observe(studioRef.current)
+    if (clientsRef.current) observer.observe(clientsRef.current)
+    if (collectionsRef.current) observer.observe(collectionsRef.current)
+    if (recognitionRef.current) observer.observe(recognitionRef.current)
+    if (transparencyRef.current) observer.observe(transparencyRef.current)
+    if (careersRef.current) observer.observe(careersRef.current)
+    if (contactRef.current) observer.observe(contactRef.current)
+    if (teamRef.current) observer.observe(teamRef.current)
+    // ... observe other sections
+
+    // Cleanup function to disconnect the observer when the component unmounts
+    return () => observer.disconnect()
+  }, [
+    studioRef,
+    teamRef,
+    clientsRef,
+    collectionsRef,
+    recognitionRef,
+    transparencyRef,
+    careersRef,
+    contactRef,
+  ])
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (navRef.current) {
+        const rect = navRef.current.getBoundingClientRect()
+        // Check if the top of the div has reached or passed the top of the viewport
+        setIsFixed(rect.top <= 35)
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll)
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+
   return (
     <div className="margined-section">
-      <h1 className={styles.title}>About</h1>
-      <div className={styles.subHeadingContainer}>
-        <a href="#studio" className={styles.studioAnchor}>
-          Studio
-        </a>
-        <a href="#team">Team</a>
-        <a href="#clients">Clients</a>
-        <a href="#collections">Collections</a>
-        <a href="#recognition">Recognition</a>
-        <a href="#transparency">Transparency</a>
-        <a href="#careers">Careers</a>
-        <a href="#contact">Contact</a>
-      </div>
-      {studioText && (
-        <motion.div
-          initial={{ opacity: 0 }}
-          transition={{ duration: 1 }}
-          whileInView={{ opacity: 1 }}
-          viewport={{ once: true }}
-          dangerouslySetInnerHTML={{
-            __html: studioText.childMarkdownRemark.html,
+      <div>
+        <h1 className={styles.title}>About</h1>
+        <div
+          ref={navRef}
+          style={{ marginBottom: isFixed ? "35px" : "0" }}
+        ></div>
+        <div
+          className={styles.subHeadingContainer}
+          style={{
+            // Initially relative, becomes fixed when scrolled to top
+            position: isFixed ? "fixed" : "relative",
+            top: isFixed ? 35 : "auto",
+            width: "100%", // Ensure it maintains full width when fixed
+            zIndex: 100, // Optional: for stacking context
+            backgroundColor: "#fff", // Optional: for visual clarity
           }}
-          className={styles.introText}
-          id="studio"
-        ></motion.div>
-      )}
-      {studioHeroImage && (
-        <motion.div
-          initial={{ opacity: 0 }}
-          transition={{ duration: 1 }}
-          whileInView={{ opacity: 1 }}
-          viewport={{ once: true }}
-          className={styles.banner}
         >
-          <GatsbyImage
-            image={studioHeroImage.image.gatsbyImageData}
-            alt={studioHeroImage.image.description}
-            className={styles.bannerImage}
-          ></GatsbyImage>
-        </motion.div>
-      )}
-
-      {studioGallery && (
-        <div className={styles.galleryContainer}>
-          {studioGallery.map((item, index) => {
-            const imgWidth = isMobile
-              ? (item.image?.width * 30) / item.image?.height
-              : (item.image?.width * 60) / item.image?.height
-            return (
-              <motion.figure
-                initial={{ opacity: 0 }}
-                transition={{ duration: 1 }}
-                whileInView={{ opacity: 1 }}
-                viewport={{ once: true }}
-                key={index}
-              >
-                <GatsbyImage
-                  image={item.image.gatsbyImageData}
-                  alt={item.image.description}
-                  className={styles.galleryImage}
-                  style={{
-                    height: isMobile ? "30vh" : "60vh",
-                    width: `${imgWidth}vh`,
-                  }}
-                ></GatsbyImage>
-                <figcaption>{item.caption}</figcaption>
-              </motion.figure>
-            )
-          })}
+          <a
+            href="#studio"
+            className={activeSection === "studio" ? styles.studioAnchor : ""}
+          >
+            Studio
+          </a>
+          <a
+            href="#team"
+            className={activeSection === "team" ? styles.studioAnchor : ""}
+          >
+            Team
+          </a>
+          <a
+            href="#clients"
+            className={activeSection === "clients" ? styles.studioAnchor : ""}
+          >
+            Clients
+          </a>
+          <a
+            href="#collections"
+            className={
+              activeSection === "collections" ? styles.studioAnchor : ""
+            }
+          >
+            Collections
+          </a>
+          <a
+            href="#recognition"
+            className={
+              activeSection === "recognition" ? styles.studioAnchor : ""
+            }
+          >
+            Recognition
+          </a>
+          <a
+            href="#transparency"
+            className={
+              activeSection === "transparency" ? styles.studioAnchor : ""
+            }
+          >
+            Transparency
+          </a>
+          <a
+            href="#careers"
+            className={activeSection === "careers" ? styles.studioAnchor : ""}
+          >
+            Careers
+          </a>
+          <a
+            href="#contact"
+            className={activeSection === "contact" ? styles.studioAnchor : ""}
+          >
+            Contact
+          </a>
         </div>
-      )}
-      <motion.p
-        initial={{ opacity: 0 }}
-        transition={{ duration: 1 }}
-        whileInView={{ opacity: 1 }}
-        viewport={{ once: true }}
-        className={styles.secondaryBigHeading}
-      >
-        Team
-      </motion.p>
-      <div className={styles.secondarySectionHeading} id="team">
-        <motion.p
-          initial={{ opacity: 0 }}
-          transition={{ duration: 1 }}
-          whileInView={{ opacity: 1 }}
-          viewport={{ once: true }}
-          className={styles.secondaryHeading}
-        >
-          Principals
-        </motion.p>
-        <div className={styles.founders}>
-          <div>
-            <motion.div
-              initial={{ opacity: 0 }}
-              transition={{ duration: 1 }}
-              whileInView={{ opacity: 1 }}
-              viewport={{ once: true }}
-            >
-              <GatsbyImage
-                image={jingLiuHeadshot.gatsbyImageData}
-                alt={jingLiuHeadshot.description}
-                className={styles.headshot}
-              ></GatsbyImage>
-            </motion.div>
-            <ExpandingText text={jingLiuBio} principal={true}></ExpandingText>
+      </div>
+      <div id="studio" ref={studioRef} className={styles.anchorSection}>
+        {studioText && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+            dangerouslySetInnerHTML={{
+              __html: studioText.childMarkdownRemark.html,
+            }}
+            className={styles.introText}
+          ></motion.div>
+        )}
+        {studioHeroImage && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+            className={styles.banner}
+          >
+            <GatsbyImage
+              image={studioHeroImage.image.gatsbyImageData}
+              alt={studioHeroImage.image.description}
+              className={styles.bannerImage}
+            ></GatsbyImage>
+          </motion.div>
+        )}
+        {studioGallery && (
+          <div className={styles.galleryContainer}>
+            {studioGallery.map((item, index) => {
+              const imgWidth = isMobile
+                ? (item.image?.width * 30) / item.image?.height
+                : (item.image?.width * 60) / item.image?.height
+              return (
+                <motion.figure
+                  initial={{ opacity: 0 }}
+                  transition={{ duration: 1 }}
+                  whileInView={{ opacity: 1 }}
+                  viewport={{ once: true }}
+                  key={index}
+                >
+                  <GatsbyImage
+                    image={item.image.gatsbyImageData}
+                    alt={item.image.description}
+                    className={styles.galleryImage}
+                    style={{
+                      height: isMobile ? "30vh" : "60vh",
+                      width: `${imgWidth}vh`,
+                    }}
+                  ></GatsbyImage>
+                  <figcaption>{item.caption}</figcaption>
+                </motion.figure>
+              )
+            })}
           </div>
-          <div>
-            <motion.div
-              initial={{ opacity: 0 }}
-              transition={{ duration: 1 }}
-              whileInView={{ opacity: 1 }}
-              viewport={{ once: true }}
-            >
-              <GatsbyImage
-                image={florianIdenburgHeadshot.gatsbyImageData}
-                alt={florianIdenburgHeadshot.description}
-                className={styles.headshot}
-              ></GatsbyImage>
-            </motion.div>
-            <ExpandingText
-              text={florianIdenburgBio}
-              principal={true}
-            ></ExpandingText>
-          </div>
-        </div>
+        )}
+      </div>
+      <div id="team" ref={teamRef} className={styles.anchorSection}>
         <motion.p
           initial={{ opacity: 0 }}
           transition={{ duration: 1 }}
           whileInView={{ opacity: 1 }}
           viewport={{ once: true }}
-          className={styles.secondaryHeading}
+          className={styles.secondaryBigHeading}
         >
-          Leadership
+          Team
         </motion.p>
-        {leadership &&
-          leadership.map(member => (
-            <motion.div
-              initial={{ opacity: 0 }}
-              transition={{ duration: 1 }}
-              whileInView={{ opacity: 1 }}
-              viewport={{ once: true }}
-            >
-              <GatsbyImage
-                image={member.headshot.gatsbyImageData}
-                alt={member.headshot.description}
-                className={styles.teamImage}
-              ></GatsbyImage>
-              <div>{member.name}</div>
-              <div>{member.title}</div>
-              <ExpandingText text={member.bio}></ExpandingText>
-            </motion.div>
-          ))}
-        <motion.p
-          initial={{ opacity: 0 }}
-          transition={{ duration: 1 }}
-          whileInView={{ opacity: 1 }}
-          viewport={{ once: true }}
-          className={styles.secondaryHeading}
-        >
-          Staff
-        </motion.p>
-        {teamMembers && (
-          <div className={styles.staffContainer}>
-            {teamMembers.map(member => (
+        <div className={styles.secondarySectionHeading}>
+          <motion.p
+            initial={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+            className={styles.secondaryHeading}
+          >
+            Principals
+          </motion.p>
+          <div className={styles.founders}>
+            <div>
               <motion.div
                 initial={{ opacity: 0 }}
                 transition={{ duration: 1 }}
                 whileInView={{ opacity: 1 }}
                 viewport={{ once: true }}
+              >
+                <GatsbyImage
+                  image={jingLiuHeadshot.gatsbyImageData}
+                  alt={jingLiuHeadshot.description}
+                  className={styles.headshot}
+                ></GatsbyImage>
+              </motion.div>
+              <ExpandingText text={jingLiuBio} principal={true}></ExpandingText>
+            </div>
+            <div>
+              <motion.div
+                initial={{ opacity: 0 }}
+                transition={{ duration: 1 }}
+                whileInView={{ opacity: 1 }}
+                viewport={{ once: true }}
+              >
+                <GatsbyImage
+                  image={florianIdenburgHeadshot.gatsbyImageData}
+                  alt={florianIdenburgHeadshot.description}
+                  className={styles.headshot}
+                ></GatsbyImage>
+              </motion.div>
+              <ExpandingText
+                text={florianIdenburgBio}
+                principal={true}
+              ></ExpandingText>
+            </div>
+          </div>
+          <motion.p
+            initial={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+            className={styles.secondaryHeading}
+          >
+            Leadership
+          </motion.p>
+          {leadership &&
+            leadership.map((member, index) => (
+              <motion.div
+                initial={{ opacity: 0 }}
+                transition={{ duration: 1 }}
+                whileInView={{ opacity: 1 }}
+                viewport={{ once: true }}
+                key={index}
               >
                 <GatsbyImage
                   image={member.headshot.gatsbyImageData}
@@ -211,13 +306,47 @@ const About = ({ data }) => {
                 ></GatsbyImage>
                 <div>{member.name}</div>
                 <div>{member.title}</div>
+                <ExpandingText text={member.bio}></ExpandingText>
               </motion.div>
             ))}
-          </div>
-        )}
+          <motion.p
+            initial={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+            className={styles.secondaryHeading}
+          >
+            Staff
+          </motion.p>
+          {teamMembers && (
+            <div className={styles.staffContainer}>
+              {teamMembers.map((member, index) => (
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  transition={{ duration: 1 }}
+                  whileInView={{ opacity: 1 }}
+                  viewport={{ once: true }}
+                  key={index}
+                >
+                  <GatsbyImage
+                    image={member.headshot.gatsbyImageData}
+                    alt={member.headshot.description}
+                    className={styles.teamImage}
+                  ></GatsbyImage>
+                  <div>{member.name}</div>
+                  <div>{member.title}</div>
+                </motion.div>
+              ))}
+            </div>
+          )}
+        </div>
       </div>
       {clients && (
-        <div className={styles.tertiarySectionHeading} id="clients">
+        <div
+          className={styles.tertiarySectionHeading}
+          id="clients"
+          ref={clientsRef}
+        >
           <motion.p
             initial={{ opacity: 0 }}
             transition={{ duration: 1 }}
@@ -240,7 +369,11 @@ const About = ({ data }) => {
         </div>
       )}
       {collections && (
-        <div className={styles.tertiarySectionHeading} id="collections">
+        <div
+          className={styles.tertiarySectionHeading}
+          id="collections"
+          ref={collectionsRef}
+        >
           <motion.p
             initial={{ opacity: 0 }}
             transition={{ duration: 1 }}
@@ -282,7 +415,11 @@ const About = ({ data }) => {
         </div>
       )}
       {recognitionPreface && (
-        <div className={styles.tertiarySectionHeading} id="recognition">
+        <div
+          className={styles.tertiarySectionHeading}
+          id="recognition"
+          ref={recognitionRef}
+        >
           <motion.p
             initial={{ opacity: 0 }}
             transition={{ duration: 1 }}
@@ -357,7 +494,11 @@ const About = ({ data }) => {
       )}
 
       {transparency && (
-        <div className={styles.tertiarySectionHeading} id="transparency">
+        <div
+          className={styles.tertiarySectionHeading}
+          id="transparency"
+          ref={transparencyRef}
+        >
           <motion.p
             initial={{ opacity: 0 }}
             transition={{ duration: 1 }}
@@ -380,7 +521,11 @@ const About = ({ data }) => {
         </div>
       )}
       {careers && (
-        <div className={styles.tertiarySectionHeading} id="careers">
+        <div
+          className={styles.tertiarySectionHeading}
+          id="careers"
+          ref={careersRef}
+        >
           <motion.p
             initial={{ opacity: 0 }}
             transition={{ duration: 1 }}
@@ -430,7 +575,11 @@ const About = ({ data }) => {
         </div>
       )}
       {contact && (
-        <div className={styles.tertiarySectionHeading} id="contact">
+        <div
+          className={styles.tertiarySectionHeading}
+          id="contact"
+          ref={contactRef}
+        >
           <motion.p
             initial={{ opacity: 0 }}
             transition={{ duration: 1 }}
